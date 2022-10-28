c84b07e1b74f6e13c213a6fe47cf549f22ab4495
chiply2002 <chiply2002@gmail.com>
2022-10-28 11:08:45 +0700

    Redux core

---
+++ src/redux/selectors.js
@@ -0,0 +1,38 @@
+import { createSelector } from "reselect";
+
+export const searchTextSelector = (state) => state.filters.search;
+export const filterStatusSelector = (state) => state.filters.status;
+export const filterprioritiesSelector = (state) => state.filters.priority;
+export const todoListSelector = (state) => state.todoList;
+
+export const todosRemainingSelector = createSelector(
+  todoListSelector,
+  filterStatusSelector,
+  filterprioritiesSelector,
+  searchTextSelector,
+  (todoList, status, priorities, searchText) => {
+    return todoList.filter((todo) => {
+      if (status === "All") {
+        return priorities.length
+          ? todo.name.includes(searchText) && priorities.includes(todo.priority)
+          : todo.name.includes(searchText);
+      }
+      return (
+        todo.name.includes(searchText) &&
+        (status === "Completed" ? todo.completed : !todo.completed) &&
+        (priorities.length ? priorities.includes(todo.priority) : true)
+      );
+    });
+  }
+);
+
+// export const todoListSelector = (state) => {
+//     const searchText = searchTextSelector(state)
+
+//     const todosRemaining = state.todoList.filter((todo) => {
+//         return todo.name.includes(searchText)
+//     })
+//     return todosRemaining
+// }
+
+// export const searchTextSelector = (state) => state.filters.search
---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+//Redux Core
+
+import { createStore } from "redux";
+import rootReducer from "./reducer";
+
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const composedEnhancers = composeWithDevTools();
+
+const store = createStore(rootReducer, composedEnhancers);
+
+//Redux Toolkits
+// import { configureStore } from "@reduxjs/toolkit";
+// import filtersReducer from "../components/Filters/FiltersSlice";
+// import todoListReducer from "../components/Todolist/TodoSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     filters: filtersReducer,
+//     todoList: todoListReducer,
+//   },
+// });
+
+export default store;
